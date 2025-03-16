a2cd190952be6378cc81d2686e680febae5707a1
ae <ae@usedesk.ru>
2025-03-17 01:03:00 +0300

    feat(6): add log context

--- rag/modules/logs/logger.py
+++ rag/modules/logs/logger.py
@@ -1,3 +1,5 @@
+import json
+from typing import Any
 from loguru import logger
 from rag.modules.logs.fake_logger import FakeLogger
 
@@ -8,17 +10,22 @@ class LoggerWrapper:
         else:
             self.logger = FakeLogger()
 
-    def debug(self, message: str) -> None:
-        self.logger.debug(message)
+    def debug(self, message: str, **context: Any) -> None:
+        self.logger.debug(self._concatinate_args(message, **context))
 
-    def info(self, message: str) -> None:
-        self.logger.info(message)
+    def info(self, message: str, **context: Any) -> None:
+        self.logger.info(self._concatinate_args(message, **context))
 
-    def warning(self, message: str) -> None:
-        self.logger.warning(message)
+    def warning(self, message: str, **context: Any) -> None:
+        self.logger.warning(self._concatinate_args(message, **context))
 
-    def error(self, message: str) -> None:
-        self.logger.error(message)
+    def error(self, message: str, **context: Any) -> None:
+        self.logger.error(self._concatinate_args(message, **context))
 
-    def critical(self, message: str) -> None:
-        self.logger.critical(message)
+    def critical(self, message: str, **context: Any) -> None:
+        self.logger.critical(self._concatinate_args(message, **context))
+
+    def _concatinate_args(self, message: str, **context: Any) -> str:
+        if context:
+            return f"{message}\n{json.dumps(context, ensure_ascii=False)}"
+        return message
